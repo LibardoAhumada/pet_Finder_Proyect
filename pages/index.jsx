36275019f87893b1ee04501d1d112647f3d8b82d
jace <jace.lant@gmail.com>
2023-12-14 22:56:51 -0600

    1st

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,27 @@
-import Causa from "../components/Causa"
-import Footer from "../components/Footer"
-import Donar from "../components/Donar"
+import Causa from "./Components/Causa";
+import Footer from "./Components/Footer";
+import Donar from "./Components/Donar";
 import Navbar from './Components/Navbar';
-import FirstText from "./Components/FirstText"
+import FirstText from "./Components/FirstText";
 import LandingImg from './Components/LandingImg';
 
 const Home = () => {
   return (
     <>
+      <div>
+        <Navbar />
+        <div>
+          <FirstText />
+        </div>
+        <div>
+          <LandingImg />
+        </div>
+      </div>
       <Causa />
       <Footer />
-      <Donar /> 
+      <Donar />
     </>
-
-  )
-}
-    <div>
-      <Navbar />
-      <div>
-        <FirstText/>
-      </div>
-      <div>
-        <LandingImg/>
-      </div>
-    </div>
   );
 };
-export default Home;
 
+export default Home;
